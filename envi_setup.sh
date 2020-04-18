6509682a3e977ea2b1fa62797792e98725023c22
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-18 21:09:20 +0300

    Update envi_setup.sh

--- envi_setup.sh
+++ envi_setup.sh
@@ -1,46 +1,24 @@
    #! /bin/bash
-   my_array=( apt python3-pip )
-   dpkg -s my_array &> /dev/null
+   
+ # my_array=( apt python3-pip )
+  #dpkg -s my_array &> /dev/null
  
- for key in "${!my_array[@]}"; do 
-  if [ $? -eq 0 ]; then
-    echo "Package  is installed + ${my_array[$key]} "
-  else
+ #for key in "${!my_array[@]}"; do 
+  #if [ $? -eq 0 ]; then
+   # echo "Package  is installed ${my_array[$key]} "
+  #else
    # echo "Package  is NOT installed!"
-    sudo apt install ${my_array[$key]}
-    fi
-  done
+   # sudo apt install ${my_array[$key]}
+    #fi
+  #done
   
-  if [dpkg  build-essential libssl-dev libffi-dev python3-dev -eq 0 ]; then
-    echo "Package  is installed  "
+    if [ apt -eq 0 ]; then
+    echo "Package  is installed apt "
   else
    # echo "Package  is NOT installed!"
-    sudo apt install build-essential libssl-dev libffi-dev python3-dev
+    sudo apt update
     fi
-    
-     if [dpkg  python3-venv -eq 0 ]; then
-    echo "Package  is installed  "
-  else
-   # echo "Package  is NOT installed!"
-    sudo apt install -y python3-venv
-    fi
-    
-    python3.6 -m venv my_env
-    source my_env/bin/activate
-     
-  if [ dpkg  gunicorn -eq 0 ]; then
-    echo "Package  is installed  "
-  else
-   # echo "Package  is NOT installed!"
-    sudo apt install gunicorn
-    fi
-    
-     if [dpkg  requirements.txt -eq 0 ]; then
-    echo "Package  is installed  "
-  else
-   # echo "Package  is NOT installed!"
-    pip install -r requirements.txt
-    fi
+  
      #sudo apt update
      #sudo apt-y upgrade
      #sudo apt install -y python3-pip
