4fa71f046c0e10a7723b569ac2b27b937caa0419
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-18 15:41:29 +0300

    Update envi_setup.sh

--- envi_setup.sh
+++ envi_setup.sh
@@ -1,12 +1,19 @@
    #! /bin/bash
    
-     sudo apt update
-     sudo apt-y upgrade
-     sudo apt install -y python3-pip
-     sudo apt install build-essential libssl-dev libffi-dev python3-dev
-     sudo apt install -y python3-venv
-     python3.6 -m venv my_env
-     source my_env/bin/activate
-     sudo apt install gunicorn
-     pip install -r requirements.txt
+   dpkg -s apt &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Package  is installed!"
+else
+    echo "Package  is NOT installed!"
+fi
+     #sudo apt update
+     #sudo apt-y upgrade
+     #sudo apt install -y python3-pip
+     #sudo apt install build-essential libssl-dev libffi-dev python3-dev
+     #sudo apt install -y python3-venv
+     #python3.6 -m venv my_env
+     #source my_env/bin/activate
+     #sudo apt install gunicorn
+     #pip install -r requirements.txt
      
