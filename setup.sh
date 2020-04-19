c0968a1dd8b4230c4c2304e902eee0c976fd7f27
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-19 16:23:54 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+      sudo apt update
+      sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+      sudo apt install python3-venv
+      mkdir ~/myproject
+      cd ~/myproject
+      python3.6 -m venv myprojectenv
+      source myprojectenv/bin/activate
+      pip install wheel
+      pip install gunicorn flask
+      pip install -r requirements.txt
