df306a3e02e93c5007baaba994183d4677670a6b
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-19 10:30:56 +0300

    Update envi_setup.sh

--- envi_setup.sh
+++ envi_setup.sh
@@ -27,7 +27,7 @@
 #done
 #sudo apt-get -qq install apt
 
-if [${dpkg-query -W -f='${Status} ${Version}\n' apt} -eq 0]; then
+if ${dpkg-query -W -f='${Status} ${Version}\n' apt} -eq 0; then
     echo "Successfully installed apt"
 else
    sudo apt install
@@ -50,11 +50,11 @@ fi
 #fi
 
 
-#if  dpkg-query -W -f='${Status} ${Version}\n' python3-venv -eq 0 ; then
- #  echo "Successfully installed python3-venv"
-#else
- # sudo apt install python3-venv
-#fi
+if  ${dpkg-query -W -f='${Status} ${Version}\n' python3-venv} -eq 0 ; then
+   echo "Successfully installed python3-venv"
+else
+  sudo apt install python3-venv
+fi
 
 #sudo  mkdir ~/myproject
 #cd ~/myproject
