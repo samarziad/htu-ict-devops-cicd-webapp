0208208cbeaef90d955e140e436513fa8a937888
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-19 22:06:07 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,13 @@
       sudo apt update
       sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
       sudo apt install python3-venv
-      mkdir ~/myproject
-      cd ~/myproject
-      sudo  python3.6 -m venv myprojectenv
-      source myprojectenv/bin/activate
+      #mkdir ~/myproject
+     # cd ~/myproject
+      cd /home/vsts/work/1/s/.git/
+      #sudo  python3.6 -m venv myprojectenv
+      #source myprojectenv/bin/activate
+      sudo  python3.6 -m venv s
+      source s/bin/activate
       pip install wheel
-      sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
-     # pip install -r requirements.txt
+      #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
+      pip install -r requirements.txt
