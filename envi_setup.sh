6a27b8580aa01481a3a8476258a3e0dab62cf10a
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-19 12:10:39 +0300

    Update envi_setup.sh

--- envi_setup.sh
+++ envi_setup.sh
@@ -36,13 +36,13 @@ fi
 
 
 
-if dpkg -s  python3-pip 2>/dev/null | grep -q "ok installed" ; then
+if dpkg -query -s  python3-pip 2>/dev/null | grep -q "ok installed" ; then
     echo "Successfully installed python3-pip"
 else
     sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools 
 fi
 
-if  dpkg-query -W -f='${Status} ${Version}\n' python3-dev 2>/dev/null | grep -q "ok installed" ; then
+if  dpkg-query -W -f '${Status} \n' python3-dev 2>/dev/null | grep -q "ok installed" ; then
     echo "Successfully installed python3-dev"
 else
     sudo apt install  python3-dev 
