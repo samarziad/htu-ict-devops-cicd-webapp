9ed36a6b357bf3e6ef2777481e9c3e8188d11e68
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 01:01:23 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash 
 
-      sudo apt update
-      sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-      sudo apt install python3-venv
-      cd ~/myproject
-      sudo cp /home/vsts/work/1/s .
+      cd htu-ict-devops-cicd-webapp 
+      #sudo apt update
+      #sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+      #sudo apt install python3-venv
+      #cd ~/myproject
+      #sudo cp /home/vsts/work/1/s .
       # mkdir ~/myproject
      
-      sudo  python3.6 -m venv myprojectenv
-      source myprojectenv/bin/activate
+      #sudo  python3.6 -m venv myprojectenv
+      #source myprojectenv/bin/activate
       #sudo  python3.6 -m venv s
       #source s/bin/activate
-      pip install wheel
+      #pip install wheel
       #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
-      pip install -r requirements.txt
+      #pip install -r requirements.txt
