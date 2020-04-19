8e54d610443cba16f4b563b3323366cc88932607
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-19 23:58:27 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@
      
       sudo  python3.6 -m venv myprojectenv
       source myprojectenv/bin/activate
-      sudo  python3.6 -m venv s
-      source s/bin/activate
+      #sudo  python3.6 -m venv s
+      #source s/bin/activate
       pip install wheel
       #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
       pip install -r requirements.txt
