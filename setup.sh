d72c445b2ff64df743b004d31e65564918e6ab3c
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 11:35:33 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@
       #cd $(Agent.BuildDirectory)
       #cd htu-ict-devops-cicd-webapp
       sudo apt update
+       sudo apt-get update
       sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
       sudo apt install python3-venv
       mkdir ~/htu-ict-devops-cicd-webapp
