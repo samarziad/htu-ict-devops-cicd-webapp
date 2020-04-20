f152820e8018ea94860b4029dd07792f51b9e066
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 03:33:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,23 @@
 
     
     #locate  \agent\_work\1\s\
-    locate   htu-ict-devops-cicd-webapp
+    #locate   htu-ict-devops-cicd-webapp
       #cd contoso-demo
       #cd c 
       #cd $(Agent.BuildDirectory)
       #cd htu-ict-devops-cicd-webapp
-      #sudo apt update
-      #sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-      #sudo apt install python3-venv
-      #cd ~/myproject
+      sudo apt update
+      sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+      sudo apt install python3-venv
+      cd ~/myproject
       #sudo cp /home/vsts/work/1/s .
       # mkdir ~/myproject
      
-      #sudo  python3.6 -m venv myprojectenv
-      #source myprojectenv/bin/activate
+      sudo  python3.6 -m venv myprojectenv
+      source myprojectenv/bin/activate
       #sudo  python3.6 -m venv s
       #source s/bin/activate
-      #pip install wheel
+      pip install wheel
       #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
-      #pip install -r requirements.txt
+      #
+       pip install -r requirements.txt
