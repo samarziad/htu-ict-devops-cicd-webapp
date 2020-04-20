4a47c6b1d247f985672857351c4d0cfe73b10664
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 11:41:42 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -4,8 +4,10 @@
       #sudo  python3.6 -m venv s
       #source s/bin/activate
       #pip3 install gunicorn
-      sudo apt-get -t stretch-backports install gunicorn
+      #sudo apt-get -t stretch-backports install gunicorn
+      sudo apt install gunicorn3
       sudo pip install wheel
       #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
        
        sudo pip install -r requirements.txt
+      sudo gunicorn --bind= 0.0.0.0:4321 --workers=4 hello:app
