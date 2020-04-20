73649a8197c1b3440160517e2edc607e9987203d
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 19:40:41 +0300

    cd ~/htu-ict-devops-cicd-webapp

--- setup.sh
+++ setup.sh
@@ -5,17 +5,29 @@
     #locate   htu-ict-devops-cicd-webapp
       #cd $(Agent.BuildDirectory)
       #cd htu-ict-devops-cicd-webapp
-      sudo apt update
-       sudo apt-get update
-      sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
-      sudo apt install python3-venv
-      mkdir ~/htu-ict-devops-cicd-webapp
-      cd ~/htu-ict-devops-cicd-webapp
-      sudo  python3.6 -m venv htu-ict-devops-cicd-webappenv
-      source htu-ict-devops-cicd-webappenv/bin/activate
+       
+       #sudo apt update
+       #sudo apt-get update
+      #sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+      #sudo apt install python3-venv
+      #mkdir ~/htu-ict-devops-cicd-webapp
+      #cd ~/htu-ict-devops-cicd-webapp
+      #sudo  python3.6 -m venv htu-ict-devops-cicd-webappenv
+      #source htu-ict-devops-cicd-webappenv/bin/activate
       #sudo cp /home/vsts/work/1/s .
       # 
-     
+     aptitude    update
+     aptitude -y upgrade
+     aptitude install python-dev
+     sudo curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python -
+     sudo curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python -
+     sudo export PATH="/usr/local/bin:$PATH"
+     sudo pip install virtualenv
+     mkdir my_app
+     cd my_app
+     virtualenv my_app_venv
+     mkdir ~/htu-ict-devops-cicd-webapp
+     cd ~/htu-ict-devops-cicd-webapp
       #sudo  python3.6 -m venv myprojectenv
       #source myprojectenv/bin/activate
       #sudo  python3.6 -m venv s
