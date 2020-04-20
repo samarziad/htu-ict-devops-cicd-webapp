cdf40a7e5037ab0ae10b5d2b7b2fa53c0b901b5a
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 10:33:51 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -2,11 +2,10 @@
 #! bin/bash 
       cd htu-ict-devops-cicd-webapp
       
-      sudo  python3.6 -m venv myprojectenv
-      source myprojectenv/bin/activate
+      
       #sudo  python3.6 -m venv s
       #source s/bin/activate
-      pip install wheel
+      sudo pip install wheel
       #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
        
-       pip install -r requirements.txt
+       sudo pip install -r requirements.txt
