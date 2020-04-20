062240c1d904794a8387abae5f3e60ce4c4c7e28
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 20:13:32 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -7,16 +7,17 @@
       #sudo apt-get -t stretch-backports install gunicorn
       #sudo apt install gunicorn3
       #python -m pip3 install --upgrade pip3
+      echo  "${PATH}"
       sudo pip install wheel
       #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
        #pip3 install --user pipenv
        #pipenv install gunicorn
        #pipenv shell
-       pip install gunicorn
-       sudo pip install -r requirements.txt
-       web: gunicorn gettingstarted.wsgi
-       heroku config:set WEB_CONCURRENCY=3
-       web: gunicorn hello:app
+       #pip install gunicorn
+       #sudo pip install -r requirements.txt
+       #web: gunicorn gettingstarted.wsgi
+       #heroku config:set WEB_CONCURRENCY=3
+       #web: gunicorn hello:app
        
         
       #gunicorn --bind= 0.0.0.0:4321 --workers=4 hello:app
