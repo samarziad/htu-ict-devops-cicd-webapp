498df493ee8f3bb726c0eee1252ef7536961e83c
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-21 01:02:26 +0300

    Update serverRun.sh

--- serverRun.sh
+++ serverRun.sh
@@ -9,6 +9,11 @@
     #cd ~/htu-ict-devops-cicd-webapp
    #gunicorn --bind= 0.0.0.0  --workers=4 hello:app
    cd  htu-ict-devops-cicd-webapp 
+    pip install gunicorn
     gunicorn hello:app -b 0.0.0.0:5000
+    sudo supervisorctl reread
+    sudo service supervisor restart
   #gunicorn -b 0.0.0.0:5000 hello:app
+   sudo nginx -t
+   sudo service nginx restart
   
