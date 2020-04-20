8d7df52eb1bfe5533c03be120d12601ed3460e15
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 11:03:22 +0300

    Update serverRun.sh

--- serverRun.sh
+++ serverRun.sh
@@ -1,11 +1,12 @@
   #!/bin/bash
   
   #/hello_app/__init__.py
-  sudo ufw allow 5000
+  #sudo ufw allow 5000
   #cd hello_app/__init__.py
-  python hello.py
-  FLASK_APP=hello.py flask run
+  #python hello.py
+  #FLASK_APP=hello.py flask run
   #bash uwsgi.ini
-  #gunicorn --bind= 0.0.0.0:4321 --workers=4 hello:app
+    cd ~/htu-ict-devops-cicd-webapp
+   gunicorn --bind= 0.0.0.0:4321 --workers=4 hello:app
   
   
