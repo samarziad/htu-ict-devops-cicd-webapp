d8ce2b2d53880b8b661a172f504214e1e9604094
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 03:07:07 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
     
-      cd  \agent\_work\1\htu-ict-devops-cicd-webapp
+      cd  $(Agent.BuildDirectory)
+      cd htu-ict-devops-cicd-webapp
       #sudo apt update
       #sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
       #sudo apt install python3-venv
