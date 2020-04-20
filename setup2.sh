ee3d1763dfef64788a14a40faeae970aa9976f91
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-20 10:49:03 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -3,6 +3,7 @@
      
       #sudo  python3.6 -m venv s
       #source s/bin/activate
+      pip install gunicorn
       sudo pip install wheel
       #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
        
