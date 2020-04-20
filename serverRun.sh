3d87ce3546c87cb69fe42b155954113c8e16da04
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-21 00:44:32 +0300

    Update serverRun.sh

--- serverRun.sh
+++ serverRun.sh
@@ -9,6 +9,6 @@
     #cd ~/htu-ict-devops-cicd-webapp
    #gunicorn --bind= 0.0.0.0  --workers=4 hello:app
    cd  htu-ict-devops-cicd-webapp 
-   gunicorn --bind=0.0.0.0  --workers=4 hello:app
+   exec gunicorn --bind=0.0.0.0  --workers=4 hello:app
   #gunicorn -b 0.0.0.0:5000 hello:app
   
