3402522e0dc984047d088f76bfed11e406c64da2
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-21 16:40:52 +0300

    Update serverRun.sh

--- serverRun.sh
+++ serverRun.sh
@@ -16,7 +16,8 @@
     sudo apt-get install supervisor
     sudo supervisorctl reread
     sudo service supervisor restart
-   
+    gunicorn --bind= 0.0.0.0  --workers=4 hello:app
+     sudo systemctl start hello.py
   #gunicorn -b 0.0.0.0:5000 hello:app
    
   
