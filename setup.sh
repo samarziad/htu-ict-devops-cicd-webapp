4b7ca77cbfe6387d196288eeb1e0d86c3fcb8705
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-22 01:29:51 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
       sudo apt-get update
       sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
       sudo apt install python3-venv
-      pip install gunicorn
+      sudo apt install gunicorn3 
      
       
      
