ecb3d8919a8089b43364ad57d1114c02439dfb41
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-22 01:43:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@
       sudo apt-get update
       sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
       sudo apt install python3-venv
+      export PATH=${PATH}:/usr/local/python3/bin
       sudo apt install gunicorn3 
       
      
