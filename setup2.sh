73a4805928c429e80f8122dd2a555b296c3a974f
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-22 00:57:39 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,35 +1,11 @@
 
 #! bin/bash 
      
-      #sudo  python3.6 -m venv s
-      #source s/bin/activate
-      #pip3 install gunicorn
-      #sudo apt-get -t stretch-backports install gunicorn
-      #sudo apt install gunicorn3
-      #python -m pip3 install --upgrade pip3
-      echo  "${PATH}"
-      sudo pip install wheel
+      sudo  python3.6 -m venv .
+      source ./bin/activate
       sudo pip install -r requirements.txt
-      sudo apt install nginx
-      sudo service nginx start
-      sudo apt-get install build-essential python
-   sudo apt-get install python-dev
- 
-      sudo ufw allow 5000
-      python  	hello.py
-      
-      #sudo service nginx start
-      #cd  htu-ict-devops-cicd-webapp 
-      #echo  "${PATH}"
-      #echo  "${ls -al}"
-      #sudo pip install gunicorn flask click itsdangerous Jinja2 MarkupSafe Werkzeug
-       #pip3 install --user pipenv
-       #pipenv install gunicorn
-       #pipenv shell
-       #pip install gunicorn
-       #sudo pip install -r requirements.txt
-       #web: gunicorn gettingstarted.wsgi
-       #heroku config:set WEB_CONCURRENCY=3
+     
+  
        #web: gunicorn hello:app
        
         
