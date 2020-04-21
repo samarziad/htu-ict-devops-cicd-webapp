740504b12328ca1602975a68594e23a97503284d
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-22 00:54:25 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,8 @@
       sudo apt-get update
       sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
       sudo apt install python3-venv
-   
+      pip install gunicorn
      
       
-      sudo  python3.6 -m venv webappenv
-      source webappenv/bin/activate
+     
 
-       
-      # pip install -r requirements.txt
