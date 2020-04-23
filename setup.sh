9b5e0b6dd6f51e94fbbfc1917e1b1b3c8c4d505f
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-23 03:44:18 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
       export PATH=${PATH}:/usr/local/python3/bin
        echo  ${PATH}
       sudo apt-get update
-      sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+      sudo apt-get -y install python3-pip
+      sudo apt install ppython3-dev build-essential libssl-dev libffi-dev python3-setuptools
       sudo apt install python3-venv
      
       sudo apt install gunicorn3 
