67430e182747230fb3f5fca7ed5e6c4854740088
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-23 03:13:26 +0300

    hello

--- serverRun.sh
+++ serverRun.sh
@@ -1,7 +1,9 @@
   #!/bin/bash
   
  sudo apt install nginx
-sudo service nginx start
+ sudo service nginx start
+ sudo ufw allow 5000
+ python hello.py
  gunicorn --bind= 0.0.0.0:5000  --workers=4 hello:app
  
   
