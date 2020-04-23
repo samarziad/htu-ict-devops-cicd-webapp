142f22ebaf9846a231a9986319ba03930adb58c2
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-23 18:13:05 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -2,12 +2,12 @@
 #! bin/bash 
      export PATH=${PATH}:/usr/local/python3/bin
         echo  ${PATH}
-      sudo -H python3.6 -m venv .
-      source ./bin/activate   && export PATH
+      sudo -H python3.6 -m venv samar
+      source samar/bin/activate   && export PATH
      
       sudo pip install -r requirements.txt
      
-  
+       echo $(ls -al)
        
         
       #gunicorn --bind= 0.0.0.0:4321 --workers=4 hello:app
