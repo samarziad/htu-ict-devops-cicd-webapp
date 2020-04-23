ebb0b2fcafac96ad3b30741b1351e742565d9512
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-23 20:55:28 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -2,8 +2,8 @@
 #! bin/bash 
      #export PATH=${PATH}:/usr/local/python3/bin
         echo  ${PATH}
-      python3.6 -m venv samar
-      source samar/bin/activate   && export PATH
+      python3 -m venv samar
+      source samar/bin/activate  # && export PATH
       pip install wheel
       
       sudo pip install -r requirements.txt
