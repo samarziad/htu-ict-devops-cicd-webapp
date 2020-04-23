eb109a9de6ab6e13d6a6dff4ec389f682ab00700
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-23 03:59:47 +0300

    Update serverRun.sh

--- serverRun.sh
+++ serverRun.sh
@@ -4,9 +4,10 @@
   echo ${PATH}
  #sudo apt install nginx
  #sudo service nginx start
+ cd ~/hello_app
  sudo ufw allow 5000
  python hello.py
- gunicorn --bind= 0.0.0.0:5000  --workers=4 hello:app
+ gunicorn --bind 0.0.0.0:5000  --workers=4 hello:app
  
   
 
