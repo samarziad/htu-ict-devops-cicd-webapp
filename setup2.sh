e8cb18b20a4da4d33bec68af03cee8b845b236ec
samarziad <61584220+samarziad@users.noreply.github.com>
2020-04-23 18:01:19 +0300

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -3,7 +3,7 @@
      export PATH=${PATH}:/usr/local/python3/bin
         echo  ${PATH}
       sudo -H python3.6 -m venv .
-      source ./bin/activate
+      source ./bin/activate   && export PATH
      
       sudo pip install -r requirements.txt
      
